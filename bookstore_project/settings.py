0f5ad0918f2c3ead7e86fede6d85946a1eccbca6
Charles M <montemayorcharles@outlook.com>
2020-05-05 20:20:02 -0600

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -143,6 +143,11 @@ STATICFILES_FINDERS = [
         "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# user
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # django-crispy-forms
